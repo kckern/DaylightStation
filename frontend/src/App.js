f3ebe8089c0dc1d736657405bd692cb72669cdb9
KC Kern <kc@kckern.com>
2023-07-24 14:09:36 +0900

    Init

---
+++ frontend/src/App.js
@@ -0,0 +1,73 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from 'react-live-clock';
+import rd3 from 'react-d3-library';
+import node from 'd3file';
+
+
+import { render } from 'react-dom'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+const RD3Component = rd3.Component;
+
+function App() {
+  return (
+    <div style={{backgroundColor:"#000", display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center'}}>
+
+        <div style={{height:"1080px",width:"1920px", backgroundColor:"#333", display: 'flex'}} className="relative">
+          <LeftSideBar/>
+          <MainContent/>
+          <RightSideBar/>
+        </div>
+    </div>
+  );
+}
+
+
+const options = {
+  title: {
+    text: 'My chart'
+  },
+  series: [{
+    data: [1, 2, 3]
+  }]
+}
+
+
+function LeftSideBar(){
+  return(
+    <div className="h-full w-1/4 bg-white" style={{padding: '3rem'}}>
+      <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
+    </div>
+  )
+}
+
+function RightSideBar(){
+
+  return(
+    <div className="h-full w-1/4 " style={{padding: '3rem'}}>
+      RIGHT
+      <HighchartsReact
+    highcharts={Highcharts}
+    options={options}
+  />
+
+
+<RD3Component data={{d3: node}} />
+
+    </div>
+  )
+}
+
+function MainContent(){
+  return(
+    <div className="h-full w-1/2 " style={{padding: '3rem'}}>
+      MAIN
+    </div>
+  )
+}
+
+
+
+export default App;
