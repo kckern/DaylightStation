857747eca2491497f73c1415de5f7a91b536c437
KC Kern <kc@kckern.com>
2023-08-11 12:03:11 +0900

    Delete dev.sh

--- dev.sh
+++
@@ -1,6 +0,0 @@
- 
-docker tag kckern/daylight-station:latest kckern/daylight-station-dev:beta
-docker tag kckern/daylight-station:latest kckern/daylight-station-dev:latest
-docker push kckern/daylight-station-dev:latest
-
-
