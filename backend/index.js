2aca6810cf542addbbb37c9c1216ed042f565182
KC Kern <kc@kckern.com>
2023-08-11 20:14:29 +0900

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const fs = require('fs');
-require('dotenv').config();
-
-
+const YAML = require('yaml')
 const path = require('path');
+const configExists = fs.existsSync('./config.yml');
+
+const localPath = path.join(__dirname, '../data_local');
+const tmpPath = path.join(__dirname, '../data_tmp');
+
+
 const app = express();
+if (configExists) {
+  process.env = { ...process.env, ...YAML.parse(fs.readFileSync('./config.yml', 'utf8')) }
 
-// Backend API
-app.get('/debug', function (req, res) {
-  res.json({ process: {env: process.env} });
+  if(fs.existsSync(localPath)) 
+  {
+    process.env.dataPath = localPath;
+  }else{
+    if(!fs.existsSync(tmpPath)) fs.mkdirSync(tmpPath);
+    process.env.dataPath = tmpPath;
+  }
 
-});
-
-
-// Frontend
-const fontendPath = path.join(__dirname, '../frontend/build');
-const frontendExists = fs.existsSync(fontendPath);
-if (frontendExists) app.use('/', express.static(fontendPath));
-else app.use('/', (_,res) => res.redirect('http://localhost:3111'));
-
-
+  // Backend API
+  app.get('/debug', function (req, res) {
+    res.json({ process: { env: process.env } });
+  });
+  // Frontend
+  const fontendPath = path.join(__dirname, '../frontend/build');
+  const frontendExists = fs.existsSync(fontendPath);
+  if (frontendExists) app.use('/', express.static(fontendPath));
+  else app.use('/', (_, res) => res.redirect('http://localhost:3111'));
+}
+else {
+  app.get("*",function (req, res) {
+    res.json({ error: 'This application is not configured yet. Please add a config.yml file to the root of the project. '   },500);
+  });
+}
 app.listen(3112);
 console.log('Listening on port 3112');
--- docker.local.sh
+++ docker.local.sh
@@ -1,7 +1,7 @@
 docker build -t kckern/daylight-station:latest .
 docker stop DaylightStation
 docker rm DaylightStation
-docker run --env-file .env -p 3113:3112 -d --name DaylightStation kckern/daylight-station:latest
+docker run --env-file .env -p 3113:3112 -d --name DaylightStation kckern/daylight-station:latest -v config.yml:/usr/src/app/config.yml
 
 ##push to hub
 #docker push kckern/daylight-station:latest
