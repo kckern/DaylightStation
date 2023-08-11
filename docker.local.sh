ba098ec3658ce9a8511fddad586d84d71b8eba2f
KC Kern <kc@kckern.com>
2023-08-11 12:01:51 +0900

    Docker Ready

--- docker.local.sh
+++ docker.local.sh
@@ -1,4 +1,7 @@
 docker build -t kckern/daylight-station:latest .
 docker stop DaylightStation
 docker rm DaylightStation
-docker run --env-file .env -p 8181:81 -d --name DaylightStation kckern/daylight-station:latest
+docker run --env-file .env -p 3113:3112 -d --name DaylightStation kckern/daylight-station:latest
+
+##push to hub
+#docker push kckern/daylight-station:latest
