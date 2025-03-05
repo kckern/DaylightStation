ea2aa3fb76d20bbe44f350554f7629fc07be672a
KC Kern <kc@kckern.com>
2025-03-05 10:46:00 -0800

    Update docker.local.sh

--- docker.local.sh
+++ docker.local.sh
@@ -4,6 +4,7 @@ docker rm daylight-station
 
 
 #push to local registry
+docker tag kckern/daylight-station:latest localhost:3113/daylight-station:latest
 
 
 
@@ -13,5 +14,8 @@ docker compose build
 docker compose up -d
 
 
+
+
 ##push to hub
-# docker push kckern/daylight-station:latest
+docker push kckern/daylight-station:latest
+ 
