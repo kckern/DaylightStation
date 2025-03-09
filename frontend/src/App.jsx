00db3ab6ad101874130f31f6b3cee3225c28e640
KC Kern <kc@kckern.com>
2025-03-08 17:32:35 -0800

    Upcoming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Clock from './modules/Time'
 import './App.css'
 import WeatherForecast from './modules/WeatherForecast'
 import Weather from './modules/Weather'
+import Upcoming from './modules/Upcoming'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,9 +11,12 @@ function App() {
   return (
     <>
     <div className='sidebar'>
-    <Clock/>
-    <Weather/>
-    <WeatherForecast />
+          <Clock/>
+          <Weather/>
+          <WeatherForecast />
+      </div>
+      <div className='content'>
+        <Upcoming />
       </div>
     </>
   )
