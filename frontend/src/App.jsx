5297b3eba5930fe9305016b7419096515e692877
KC Kern <kc@kckern.com>
2025-03-19 14:49:56 -0700

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,7 @@ import WeatherForecast from './modules/WeatherForecast'
 import Weather from './modules/Weather'
 import Upcoming from './modules/Upcoming'
 import Health from './modules/Health'
-import { buildDayToDayBudgetOptions } from './budget/blocks/daytoday'
-import HighchartsReact from 'highcharts-react-official'
-import Highcharts from 'highcharts'
-import { DaylightAPI } from './lib/api.mjs'
-
+import { FinanceChart } from './modules/Finance'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -34,53 +30,6 @@ function App() {
       </div>
     </div>
   )
-}
-
-
-function FinanceChart()
-{
-  const [monthData, setMonthData] = useState({})
-  useEffect(() => {
-    DaylightAPI('/data/budget/daytoday')
-      .then((data) => {
-        setMonthData(data)
-      })
-  }
-  )
-  if(!monthData) return null;
-  const options = buildDayToDayBudgetOptions(monthData, null, {plotLineColor: '#444'});
-  if(!options.chart) return null;
-  options.chart.backgroundColor = 'transparent';
-  options.title = {text: ''};
-  options.subtitle = {text: ''};
-  options.yAxis.labels.style = {color: '#FFFFFFBB', fontWeight: 'bold', fontFamily: 'Roboto Condensed', fontSize: '1rem'};
-  options.xAxis.labels.style = {color: '#FFFFFFBB', fontWeight: 'bold', fontFamily: 'Roboto Condensed', fontSize: '1rem'};
-  options.xAxis.labels.rotation = -35;
-  //vertical tick color
-  options.xAxis.tickColor = '#000';
-
-const budgetBlockDimensions = {width: 550, height : 400}
-  return (
-    <div className="budget-block">
-      <h2>Day-to-day Spending</h2>
-      <div className="budget-block-content">
-        <HighchartsReact
-          className="budget-burn-down-chart"
-          highcharts={Highcharts}
-          options={{
-            ...options,
-            chart: {
-              ...options.chart,
-              width: budgetBlockDimensions.width,
-              height: budgetBlockDimensions.height
-            }
-          }}
-        />
-      </div>
-    </div>
-  );
-
-  return 
 }
 
 
---
+++ frontend/src/modules/Finance.jsx
@@ -0,0 +1,103 @@
+
+import { buildDayToDayBudgetOptions } from '../budget/blocks/daytoday'
+import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts'
+import { DaylightAPI } from '../lib/api.mjs'
+import { useEffect, useState } from 'react'
+import './Finance.scss'
+import { formatAsCurrency } from '../budget/blocks'
+import upArrow from '../assets/icons/upGreen.svg';
+import downArrow from '../assets/icons/downRed.svg';
+
+export function FinanceChart()
+{
+  const [monthData, setMonthData] = useState({})
+  useEffect(() => {
+    DaylightAPI('/data/budget/daytoday')
+      .then((data) => {
+        setMonthData(data)
+      })
+  }
+  )
+  if(!monthData) return null;
+  const options = buildDayToDayBudgetOptions(monthData, null, {plotLineColor: '#444'});
+  if(!options.chart) return null;
+  options.chart.backgroundColor = 'transparent';
+  options.title = {text: ''};
+  options.subtitle = {text: ''};
+  options.yAxis.labels.style = {color: '#FFFFFFBB', fontWeight: 'bold', fontFamily: 'Roboto Condensed', fontSize: '1rem'};
+  options.xAxis.labels.style = {color: '#FFFFFFBB', fontWeight: 'bold', fontFamily: 'Roboto Condensed', fontSize: '1rem'};
+  options.xAxis.labels.rotation = -35;
+  //vertical tick color
+  options.xAxis.tickColor = '#000';
+  options.chart.marginTop = 10;
+
+  //2px solid white line on x axis
+
+const budgetBlockDimensions = { height : 400}
+//...monthlyBudget[month], balance, spent, daysRemaining, dailySpend, dailyBudget, dailyAdjustment: adjustPercentage, adjustPercentage
+ 
+  const info = {
+    budget: monthData.budget,
+    remaining: monthData.balance,
+    spent: monthData.spent,
+    spentPercentage: monthData.spent / monthData.budget,
+    dailySpending: monthData.dailySpend,
+    daysRemaining: monthData.daysRemaining,
+    dailyBudget: monthData.dailyBudget,
+    adjust: Math.round(monthData.dailyAdjustment)
+
+
+  }
+    const adjustIcon = monthData.dailyAdjustment > 0 ? 
+    <img src={upArrow} alt="up" style={{height: "1.3em", marginBottom: "-0.3em"}} /> : 
+    <img src={downArrow} alt="down" style={{height: "1.3em", marginBottom: "-0.3em"}} />;
+return (
+    <div className="finance">
+    <table style={{width: "100%", borderCollapse: "collapse"}}>
+        <thead style={{textAlign: "left"}}>
+            <tr>
+            <th style={{border: "1px solid black", width: "20%", padding: "8px"}}>Budget</th>
+            <th style={{border: "1px solid black", width: "20%", padding: "8px"}} colSpan={2}>Spent</th>
+                <th style={{border: "1px solid black", width: "20%",  padding: "8px"}}>Daily Spend</th>
+                <th style={{border: "1px solid black", width: "20%",  padding: "8px"}}>Balance</th>
+                <th style={{border: "1px solid black", width: "20%",  padding: "8px"}}>Days Left</th>
+                <th style={{border: "1px solid black", width: "20%",  padding: "8px"}}>Daily Budget</th>
+                <th style={{border: "1px solid black", width: "20%",  padding: "8px"}}>Adjustment</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style={{border: "1px solid black", padding: "8px"}}>{formatAsCurrency(info.budget)}</td>
+                <td style={{border: "1px solid black", padding: "8px"}}>{formatAsCurrency(info.spent)}</td>
+                <td style={{border: "1px solid black", padding: "8px"}}>{Math.round(info.spentPercentage * 100)}%</td>
+                <td style={{border: "1px solid black", padding: "8px"}}>{formatAsCurrency(info.dailySpending)}</td>
+                <td style={{border: "1px solid black", padding: "8px"}}>{formatAsCurrency(info.remaining)}</td>
+                <td style={{border: "1px solid black", padding: "8px"}}>{info.daysRemaining}</td>
+                <td style={{border: "1px solid black", padding: "8px"}}>{formatAsCurrency(info.dailyBudget)}</td>
+                <td style={{border: "1px solid black", padding: "8px"}}>{info.adjust}% {adjustIcon}</td>
+
+            </tr>
+        </tbody>
+    </table>
+      <div className="budget-block-content">
+        <HighchartsReact
+          className="budget-burn-down-chart"
+          highcharts={Highcharts}
+          options={{
+            ...options,
+            chart: {
+              ...options.chart,
+              width: budgetBlockDimensions.width,
+              height: budgetBlockDimensions.height
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  return 
+}
+
