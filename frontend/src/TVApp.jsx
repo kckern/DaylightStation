a79a491da96cd57ecae166b69ec67022cdb9386d
KC Kern <kc@kckern.com>
2025-04-02 23:52:47 -0700

    rate

--- frontend/src/TVApp.jsx
+++ frontend/src/TVApp.jsx
@@ -21,11 +21,11 @@ const TVApp = () => {
 
     const initialMenuList = [
         { title: 'D&C', key: 'scripture', value: `d&c ${Math.floor(Math.random() * 132) + 1}` },
-        { title: 'Bible Project', key: 'player', value: { plexId: 463232 } },
+        { title: 'Bible Project', key: 'player', value: { plexId: 463232, rate: 1 } },
         { title: 'Bible', key: 'list', value: { plexId: '177777' } },
         { title: 'Crash Course Kids', key: 'list', value: { plexId: '375840' } },
         { title: 'Cooking', key: 'list', value: { plexId: '416408' } },
-        { title: 'Classical', key: 'list', value: { plexId: '489862' } },
+        { title: 'Classical', key: 'player', value: { plexId: '489862', rate: 1 } },
     ];
 
     const [currentComponent, setCurrentComponent] = useState(
@@ -52,6 +52,7 @@ const TVApp = () => {
                 event.preventDefault();
                 event.returnValue = ''; // Required for some browsers to show the confirmation dialog
                 backFunction();
+                window.history.pushState(null, '', window.location.href);
                 return false; // Prevent the default action
             }
             return false; // Prevent the default action
