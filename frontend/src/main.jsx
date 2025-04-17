aecc1be74c19a1431058fbe6e9628cdf93f984b2
KC Kern <kc@kckern.com>
2025-04-16 18:10:14 -0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,32 @@ import TVApp from './TVApp.jsx';
 import FinanceApp from './budget/index.jsx'; // Step 5: Import the new component
 import './index.css';
 
+
+function CatchAll() {
+  return (
+    <div style={{ 
+      // flex, vertical center, horizontal center
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      width: '100vw',
+      fontSize: '2rem',
+      color: '#FFF',
+    }}>
+      {/* Show host, path, parameters */}
+      <pre>
+        {JSON.stringify({
+          url: window.location.href,
+          host: window.location.host,
+          path: window.location.pathname,
+          searchParams: Object.fromEntries(new URLSearchParams(window.location.search))
+        }, null, 2)}
+      </pre>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter> {/* Step 3: Wrap with BrowserRouter */}
@@ -14,7 +40,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/budget" element={<FinanceApp />} /> {/* Example of another route */}
         <Route path="/finances" element={<FinanceApp />} /> {/* Example of another route */}
         <Route path="/tv" element={<TVApp />} /> {/* Example of another route */}
-        {/* Add more <Route> components as needed */}
+        {/* Evertyhign else */}
+        <Route path="*" element={<CatchAll />} /> {/* Fallback route */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
