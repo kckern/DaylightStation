805bc8e486ab9b7095f91d592568b30c0cf65b62
KC Kern <kc@kckern.com>
2025-04-24 15:58:50 -0700

    barcode integration

--- backend/api.mjs
+++ backend/api.mjs
@@ -2,7 +2,7 @@
 import express from 'express';
 import { processWebhookPayload } from './journalist/telegram_hook.mjs';
 import {processFoodLogHook} from './journalist/foodlog_hook.mjs';
-import {foodReport} from './journalist/report.mjs';
+import {foodReport, scanBarcode} from './journalist/report.mjs';
 const apiRouter = express.Router();
 apiRouter.use(express.json({
     strict: false // Allows parsing of JSON with single-quoted property names
@@ -12,6 +12,7 @@ apiRouter.use(express.json({
 apiRouter.all(  '/journalist',    processWebhookPayload);
 apiRouter.all(  '/foodlog',       processFoodLogHook);
 apiRouter.all(  '/foodreport',    foodReport);
+apiRouter.all(  '/barcode',         scanBarcode);
 apiRouter.all(  '/*',        async (req, res) => {
     return res.status(404).json({error: `Invalid endpoint. You tried to access ${req.method} ${req.originalUrl} but this endpoint does not exist. `});
 });
