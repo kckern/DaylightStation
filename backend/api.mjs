fc40cfde74793f69751f6273f95b5b926dd67571
KC Kern <kc@kckern.com>
2025-06-07 14:19:19 -0700

    webhook set

--- backend/api.mjs
+++ backend/api.mjs
@@ -3,6 +3,7 @@ import express from 'express';
 import { processWebhookPayload } from './journalist/telegram_hook.mjs';
 import {processFoodLogHook} from './journalist/foodlog_hook.mjs';
 import {foodReport, scanBarcode} from './journalist/food_report.mjs';
+import { updateWebhook } from './journalist/lib/telegram.mjs';
 const apiRouter = express.Router();
 apiRouter.use(express.json({
     strict: false // Allows parsing of JSON with single-quoted property names
@@ -13,6 +14,22 @@ apiRouter.all(  '/journalist',    processWebhookPayload);
 apiRouter.all(  '/foodlog',       processFoodLogHook);
 apiRouter.all(  '/foodreport',    foodReport);
 apiRouter.all(  '/barcode',         scanBarcode);
+apiRouter.all('/:env(dev|prod)', async (req, res) => {
+    const env = req.params.env;
+    const journalistHook = env === 'dev' ? process.env.journalist.journalist_dev_hook : process.env.journalist.journalist_prod_hook;
+    const nutribotHook = env === 'dev' ? process.env.journalist.nutribot_dev_hook : process.env.journalist.nutribot_prod_hook;
+
+    const journalistWebhookResult = await updateWebhook(process.env.TELEGRAM_JOURNALIST_BOT_TOKEN, journalistHook);
+    const nutribotWebhookResult = await updateWebhook(process.env.TELEGRAM_NUTRIBOT_TOKEN, nutribotHook);
+
+    res.status(200).json({ 
+        message: `${env.charAt(0).toUpperCase() + env.slice(1)} webhooks updated successfully.`,
+        results: {
+            journalistWebhook: journalistWebhookResult,
+            nutribotWebhook: nutribotWebhookResult
+        }
+    });
+});
 apiRouter.all(  '/*',        async (req, res) => {
     return res.status(404).json({error: `Invalid endpoint. You tried to access ${req.method} ${req.originalUrl} but this endpoint does not exist. `});
 });
