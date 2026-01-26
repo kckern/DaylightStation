9b6250fb742e65748f6b92b326c6c2857d58d48d
KC Kern <kc@kckern.com>
2026-01-26 15:49:22 -0800

    feat(system): add services barrel export

---
+++ backend/src/0_system/services/index.mjs
@@ -0,0 +1,5 @@
+// backend/src/0_system/services/index.mjs
+
+export { IHttpClient } from './IHttpClient.mjs';
+export { HttpClient } from './HttpClient.mjs';
+export { HttpError } from './HttpError.mjs';
