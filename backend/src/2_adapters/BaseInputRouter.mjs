c6437555a0cf28b05906d1d9917d7a9ec943aae8
KC Kern <kc@kckern.com>
2026-01-29 19:13:30 -0800

    fix: add explicit index.mjs to #system/utils/errors import

--- backend/src/2_adapters/BaseInputRouter.mjs
+++ backend/src/2_adapters/BaseInputRouter.mjs
@@ -1,7 +1,7 @@
 // backend/src/2_adapters/BaseInputRouter.mjs
 
 import { InputEventType } from './telegram/IInputEvent.mjs';
-import { InfrastructureError } from '#system/utils/errors';
+import { InfrastructureError } from '#system/utils/errors/index.mjs';
 
 /**
  * Abstract base class for bot input routers.
